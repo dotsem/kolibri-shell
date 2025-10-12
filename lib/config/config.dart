2c7ece03da734d566ed4da8233058eb281ab62c0
Sem VB <sem.van.broekhoven@gmail.com>
2025-10-12 13:25:47 +0200

    update config paths

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,3 +1,3 @@
 const int workspacesPerMonitor = 10;
-const String hyprFlutterPath = "~/prog/Flutter/hypr_flutter";
+const String hyprFlutterPath = "~/prog/flutter/hypr_flutter";
 const String buildPath = "${hyprFlutterPath}/build/linux/x64/release/bundle/hypr_flutter";
